c7a773751cc2b1f94e5f3b0084a7b5581395eba5
Scott Regenthal <scottregenthal@gmail.com>
2019-10-19 13:37:42 -0600

    Update seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Student.destroy_all
+Instructor.destroy_all
 
-student_1 = Student.create(name: 'Jimmi', cohort: '1906', age: 29)
-student_2 = Student.create(name: 'Janis', cohort: '1908', age: 25)
-student_3 = Student.create(name: 'Amy', cohort: '1909', age: 27)
+# Students
+student_1 = Student.create(name: 'Lavender Brown', cohort: '1906', age: 30)
+student_2 = Student.create(name: 'Fleur Delacour', cohort: '1908', age: 24)
+student_3 = Student.create(name: 'Marietta Edgecombe', cohort: '1909', age: 28)
+student_4 = Student.create(name: 'Lee Jordan', cohort: '1906', age: 23)
+student_5 = Student.create(name: 'Remus Lupin', cohort: '1908', age: 33)
+student_6 = Student.create(name: 'Cormac McLaggen', cohort: '1909', age: 37)
 
-instructor_1 = Instructor.create(name: 'Athos')
-instructor_2 = Instructor.create(name: 'Porthos')
-instructor_2 = Instructor.create(name: 'Aramis')
+# Instructors
+instructor_1 = Instructor.create(name: 'Septima Vector')
+instructor_2 = Instructor.create(name: 'Pomona Sprout')
+instructor_3 = Instructor.create(name: 'Quirinus Quirrell')
 
-instructor_1 = student_1.instructors.create!(name: 'Athos')
-instructor_2 = student_2.instructors.create!(name: 'Porthos')
-instructor_3 = student_3.instructors.create!(name: 'Aramis')
+# InstructorStudents
+instructor_student_1 = InstructorStudent.create(student_id: student_1.id, instructor_id: instructor_1.id)
+instructor_student_2 = InstructorStudent.create(student_id: student_2.id, instructor_id: instructor_2.id)
+instructor_student_3 = InstructorStudent.create(student_id: student_3.id, instructor_id: instructor_3.id)
+instructor_student_4 = InstructorStudent.create(student_id: student_4.id, instructor_id: instructor_1.id)
+instructor_student_5 = InstructorStudent.create(student_id: student_5.id, instructor_id: instructor_2.id)
+instructor_student_6 = InstructorStudent.create(student_id: student_6.id, instructor_id: instructor_3.id)
