015d7f51922e4c374576569411495c335eb3dd9b
Scott Regenthal <scottregenthal@gmail.com>
2019-10-19 13:40:35 -0600

    Add validations to InstructorStudent join table

--- app/models/instructor_student.rb
+++ app/models/instructor_student.rb
@@ -1,4 +1,6 @@
 class InstructorStudent < ApplicationRecord
   belongs_to :instructor
   belongs_to :student
+
+  validates_presence_of :instructor_id, :student_id
 end
--- spec/models/instructor_student_spec.rb
+++ spec/models/instructor_student_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe InstructorStudent do
+  describe 'validations' do
+    it { should validate_presence_of :instructor_id }
+    it { should validate_presence_of :student_id }
+  end
+
   describe 'relationships' do
     it { should belong_to :instructor }
     it { should belong_to :student }
